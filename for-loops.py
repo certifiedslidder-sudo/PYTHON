30e268aa8fad38407d85e12df559a9357c96da6c
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-01-12 23:07:34 +0530

    learning for loops

--- for-loops.py
+++ for-loops.py
@@ -1,18 +1,23 @@
-"""name = 'sneha'
+  #iterating over a string
+name = 'sneha'
 for i in name:
     print(i)
     if(i == "e"):
-        print("this is something fab")"""
+        print("this is something fab")
         
-        
-""" colors = ["red", "green","blue","yellow"]
+        #iterating over a list
+colors = ["red", "green","blue","yellow"]
 for colors in colors: 
-    print(colors)
+    print(colors)     #this prints the elements of a list
     for i in colors:
-      print(i)"""
+      print(i)  #this will print word wise the whole list
     
-"""for k in range(5):
-    print(k+1)"""
+        #range()
+for k in range(5):
+    print(k+1)
         
-for k in range(1,11,3):
+for k in range(1,9):
+    print(k+1)
+    
+for k in range(1,11,3):  #start,stop,step...here 3 is step therefore range jumps 3 steps sarting from 1 to 11
     print(k)

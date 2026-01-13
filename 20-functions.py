9619715daf7af7b0bf53c2052525408da391f73d
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-01-13 22:32:49 +0530

    about function

---
+++ 20-functions.py
@@ -0,0 +1,33 @@
+def calculateGmean(a,b):
+    gmean = (a*b)/(a+b)
+    print(gmean)
+    
+def isGreater(a,b):
+    if(a>b):
+     print("first number is greater")
+    else:
+     print("second number is greater or equal")
+       
+def isLesser(a,b):
+    pass
+    
+    
+a =6
+b=5
+if(a>b):
+ isGreater(a,b)
+"""   print("first number is greater")
+else:
+    print("second number is greater ")"""
+#gmean1 = ( a*b)/(a+b)
+#print( gmean1)
+calculateGmean(a,b)
+c=8
+d=74
+isGreater(c,d)
+""" print("first number is greater")
+else:
+    print("second number is greater"""
+#gmean2=(c*d)/(c+d)
+#print(gmean2)
+calculateGmean(c,d)

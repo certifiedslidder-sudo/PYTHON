7c5645568f84e3d96d1a42e17403a43e0a7b8440
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-01-20 14:14:23 +0530

    f string

--- 28-f-strings.py
+++ 28-f-strings.py
@@ -10,4 +10,17 @@ name="sneha"
 print(letter.format(country, name))
 print(f"hey my name is {name} and i am from {country}")
 
-txt = "for only {}"
+txt = "for only {price:.2f} dollars!"
+print(txt.format(price = 49.09999))
+
+price = 49.09999
+txt = f"for only {price:.2f} dollars!"
+print(txt)
+#print(txt.format(price = 49.09999))
+
+print(f"{2 * 30}")   #we can use it in single statement as well..
+
+print(type(f"{2 * 30}") )  # printing data type
+
+print(f" we use f strings like this : hey my name is {{name}} and i am from {{country}}")
+#  {{}} does literal printing as it is ; it retains f string as a f string.

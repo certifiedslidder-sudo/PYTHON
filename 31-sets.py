177d5eeaa4b1314ccc04d6d82161dfe0a6bd63e3
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-01-21 22:22:09 +0530

    sets

--- 31-sets.py
+++ 31-sets.py
@@ -4,9 +4,7 @@ info = {"carter", 19,False, 5.808,87}
 print(info)             #no gurantee of order
   # therforre cannot be accessed using index numbers.
   
-  
-  
-  
+
   #QUICK QUIZ = TRY TO CREATE AN EMPTY SET. CHECK USING THE TYPE() FUNCTION WHETHER THE TYPE OF YOUR VARIABLE IS A SET..
 sneha = set()
 print(type(sneha))
@@ -14,3 +12,7 @@ print(type(sneha))
 #ACCESSING SET ITEMS:
 for value in info:
     print(value)
+    
+    
+    
+    

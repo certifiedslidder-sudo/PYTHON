dfcd7cd2d1bfcc8d0a0df60c3aa3e854e5c93400
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-01-21 19:24:50 +0530

    sets in python

---
+++ 31-sets.py
@@ -0,0 +1,16 @@
+s = {2,6,8,2,3}    #sets are unordered collection of data type..    
+print(s)           #sets are immutable..
+info = {"carter", 19,False, 5.808,87}
+print(info)             #no gurantee of order
+  # therforre cannot be accessed using index numbers.
+  
+  
+  
+  
+  #QUICK QUIZ = TRY TO CREATE AN EMPTY SET. CHECK USING THE TYPE() FUNCTION WHETHER THE TYPE OF YOUR VARIABLE IS A SET..
+sneha = set()
+print(type(sneha))
+
+#ACCESSING SET ITEMS:
+for value in info:
+    print(value)

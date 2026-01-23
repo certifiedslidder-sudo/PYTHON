9f1e5ba13d3fc639dc67fd05b1d352327e8822c6
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-01-23 22:00:03 +0530

    38-raising-custom-errors

---
+++ 38-raising-custom-errors.py
@@ -0,0 +1,10 @@
+"""a = int(input("enter any value between 5 and 9"))
+if(a<5 or a>9):
+    raise ValueError("value should be between 5 and 9")"""
+
+a= int(input("enter value"))
+if( a=quit):
+    print(set())
+else:
+    raise ValueError("value is not quit")    
+    

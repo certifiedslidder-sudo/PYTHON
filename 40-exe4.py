cfe0cc7a93efe9670ba1f9de4623d0da3ee6aa59
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-01-24 22:34:01 +0530

    exe4

---
+++ 40-exe4.py
@@ -0,0 +1,13 @@
+#write a python program to translate a message into a secret code language.use the rules below to translate normal english into secret code language.
+
+#coding:
+#if the word contains atleats three characters, remove the first letter and append it at the end.
+#now append three random characters at the starting and the end.
+#else:
+    #simply reverse the string
+    
+#decoding:
+ #if the word contains less than 3 characters, reverse it.
+ #else:
+ #remove three random characters from start and end. now remove the last letter and append it to the beginning
+ #your program should ask whether you want to code or decode.       

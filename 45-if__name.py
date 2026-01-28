c2746373cbbf41983fe7a733850627c2fc55617b
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-01-28 22:50:19 +0530

    importing file from another file

---
+++ 45-if__name.py
@@ -0,0 +1,12 @@
+import sneha2
+
+sneha2.welcome()  #2 baar print hoga   , ye wala welcome execute hora h ek sneha2 wala welcome execute ho rha hai
+
+
+import sneha2
+
+#sneha2.welcome()  sneha ke andr ka welcome ab bhi execute hoga 
+
+
+#SOLUTION:if __name__ == "__main__":           its  a check point
+#.....it is a  common idiom pattern used in python scripts to determine whether the script is being run directly or being imported as a module into another script.......
---
+++ sneha2.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("hey you are welcome from sneha")
+  
+print(__name__)  
+  #SOLUTION
+if __name__ == "__main__":  
+   welcome()    
+

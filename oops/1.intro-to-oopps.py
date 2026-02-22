5298ebfdf4b68df906c5939500216be3bb8edbc1
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-02-22 22:27:47 +0530

    entering-oops

---
+++ oops/1.intro-to-oopps.py
@@ -0,0 +1,5 @@
+RailwayForm   -->class[blueprint]
+harry         -->harry ki info wala form-->object[entity]
+tom           -->tom ki info wala form -->object[entity]
+shubham       -->shubham ki info wala form -->object[entity]
+#shubham.changeName("Shubhi")

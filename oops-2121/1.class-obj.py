7db068decaeb79e7836d7c90210d507e912607b0
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-03-03 12:52:08 +0530

    1.oops2.o

---
+++ oops-2121/1.class-obj.py
@@ -0,0 +1,18 @@
+class Employee:
+    language="py"   #this nis a class attribute
+    salary=1200000
+harry=Employee()
+harry.name="harry"  #this is an object/instance attribute
+print(harry.name,harry.language,harry.salary)
+harr=Employee()
+harr.name="harr"
+print(harr.name,harr.language,harr.salary)
+arry=Employee()
+arry.name="arry"
+print(arry.name,arry.language,arry.salary)
+rry=Employee()
+rry.name="rry"
+print(rry.name,rry.language,rry.salary)
+     
+    #name= object attribute
+    #lang,salary=class attribute

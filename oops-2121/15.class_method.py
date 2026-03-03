e10c66785cb2277de12186480b949cd450d3bd82
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-03-03 20:49:08 +0530

    16.oops(class-methods)2.o

---
+++ oops-2121/15.class_method.py
@@ -0,0 +1,22 @@
+class Employee:
+    a=1
+    @classmethod
+    def show(cls):
+        print(f"the class attribute of a is {cls.a}")
+        
+e= Employee()
+e.a = 45
+e.show()  
+'''
+self = WO OBJECT JIS MAI METHOD CHAL RHA HAI
+cls = WO CLASS JISKA WO OBJECT HAI JIMAI  WO METHOD CHAL RHA HAI
+'''
+      
+      
+# class Employee:
+#     a=1
+#     def show(self):  
+#         print(f"the class attribute of a is {self.a}")
+# e= Employee()
+# e.a = 45
+# e.show()      

248ae19bc3ddc51ec415d30f51829ae4984346bd
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-03-08 23:56:25 +0530

    dunder str , len

--- oops-2121/17.operator-overloading.py
+++ oops-2121/17.operator-overloading.py
@@ -1,3 +1,10 @@
+"""
+Operators in Python can be overloaded using dunder methods.
+These methods are called when a given operator is used on the objects.
+Operators in Python can be overloaded 
+""" 
+# __str__()   -->> used to set what gets displayed upon calling str(obj)
+#__len__()  --->> used to set what gets displayed upon calling.__len__() or len(obj)
 class Number:
     def __init__(self,n):
         self.n= n 

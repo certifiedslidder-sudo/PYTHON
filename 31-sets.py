1a19e61d4b23964eab5df799089f8c5519a72ba7
certifiedslidder-sudo <certifiedslidder@gmail.com>
2026-03-20 22:22:39 +0530

    setS are MUTABLE.....

--- 31-sets.py
+++ 31-sets.py
@@ -1,3 +1,23 @@
+"""
+      ******                                                                                           ******
+              in python, SETS ARE MUTABLE............BUT SET ELEMENTS ARE IMMUTABLE(INT,FLOAT,TUPLE)
+                     
+que   --->   why sets elements should be mutable???????
+ans   --->   because python uses hashing mechanism  to store elements.
+                     
+que ---> what is immutable???
+ans ---> means when value of obj cannot be changed after it is created.
+                              
+"""
+a=10         
+a=20  #here a is reassigned, but int 10 never itself changes . integers are immutable....
+print(a)
+print(a,a)
+
+
+
+
+
 
        #SETS DO NOT CONTAIN DUPLICATE ENTRIES LIKE LIST.
 a=set()   #empty set
@@ -6,7 +26,7 @@ c={'sanjay', 35, 446.899}     # set with multiple item
 d={100,100,100,100}         #only one 100 gets stored.
 
 s = {2,6,8,2,3}    #sets are unordered collection of data type..    
-print(s)           #sets are immutable..
+print(s)           #sets are mutable..
 info = {"carter", 19,False, 5.808,87}
 print(info)             #no gurantee of order
   # therforre cannot be accessed using index numbers.
